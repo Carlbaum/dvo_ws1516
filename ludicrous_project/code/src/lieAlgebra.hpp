2f4742cf47c586e80dd9e4707268519f82c79e4d
Oskar Carlbaum <oskca029@student.liu.se>
2016-04-06 12:58:18 +0200

    Reversed the order of the twist coordinates, now (v,w) instead of (w,v)

--- ludicrous_project/code/src/lieAlgebra.hpp
+++ ludicrous_project/code/src/lieAlgebra.hpp
@@ -6,7 +6,7 @@
 using namespace Eigen;
 /**
  * Convert xi twist coordinates to rotation matrix and translation vector
- * @param xi twist coordinates array of the form (w1, w2, w3, v1, v2, v3)
+ * @param xi twist coordinates array of the form (v1, v2, v3, w1, w2, w3)
  * @param R  output rotation matrix
  * @param t  output translation vector
  */
@@ -23,33 +23,33 @@ using namespace Eigen;
      if (norm_w > 0) {
          // log(R) = w_hat
          w_hat = norm_w/(2*sinf(norm_w)) * (R-R.transpose());
-         // v = xi.tail(3) = inverse(A) * t
-         xi.tail(3) = ( Matrix3f::Identity(3,3)
+         // v = xi.head(3) = inverse(A) * t
+         xi.head(3) = ( Matrix3f::Identity(3,3)
                         + (1-cosf(norm_w))*w_hat /(norm_w*norm_w)
                         + (norm_w-sinf(norm_w)) * w_hat * w_hat / (norm_w * norm_w * norm_w)
                       ).inverse() // a^-1 matrix
                       * t; // * t
      } else {
          w_hat = Matrix3f::Zero(3,3);
-         xi.tail(3) = t;
+         xi.head(3) = t;
      }
 
-     xi.head(3) << w_hat(2,1) , w_hat(0,2) , w_hat(1,0);
+     xi.tail(3) << w_hat(2,1) , w_hat(0,2) , w_hat(1,0);
      return xi;
  }
 
 Matrix4f lieExp(const Vector6f &xi) {
-    float norm_w = xi.head(3).norm();
+    float norm_w = xi.tail(3).norm();
     Matrix3f w_hat;
     Matrix3f R;
     Vector3f t;
-    w_hat <<    0 , -xi(2),  xi(1),
-             xi(2),     0 , -xi(0),
-            -xi(1),  xi(0),     0;
+    w_hat <<    0 , -xi(5),  xi(4),
+             xi(5),     0 , -xi(3),
+            -xi(4),  xi(3),     0;
 
     // do not divide by zero if norm_w=0
     if (norm_w > 0) {
-        // R = exp(w_hat), t = A*v, where v = xi.tail(3)
+        // R = exp(w_hat), t = A*v, where v = xi.head(3)
         R = Matrix3f::Identity(3,3)
             + sinf(norm_w) / norm_w * w_hat
             + (1-cosf(norm_w)) / (norm_w * norm_w) * w_hat*w_hat;
@@ -57,11 +57,11 @@ Matrix4f lieExp(const Vector6f &xi) {
               + (1-cosf(norm_w)) / (norm_w * norm_w) * w_hat
               + (norm_w-sinf(norm_w)) / (norm_w * norm_w *norm_w) * (w_hat * w_hat)
             ) // A matrix
-            * xi.tail(3); // * v
+            * xi.head(3); // * v
     } else {
         // R = I, A = I
         R = Matrix3f::Identity(3,3);
-        t = xi.tail(3);
+        t = xi.head(3);
     }
     Matrix4f output;
     output << R(0,0), R(0,1), R(0,2), t(0),
@@ -71,15 +71,15 @@ Matrix4f lieExp(const Vector6f &xi) {
     return output;
 }
 void convertSE3ToT(const Vector6f &xi, Matrix3f &R, Vector3f &t) {
-    float norm_w = xi.head(3).norm();
+    float norm_w = xi.tail(3).norm();
     Matrix3f w_hat;
-    w_hat <<    0 , -xi(2),  xi(1),
-             xi(2),     0 , -xi(0),
-            -xi(1),  xi(0),     0;
+    w_hat <<    0 , -xi(5),  xi(4),
+             xi(5),     0 , -xi(3),
+            -xi(4),  xi(3),     0;
 
     // do not divide by zero if norm_w=0
     if (norm_w > 0) {
-        // R = exp(w_hat), t = A*v, where v = xi.tail(3)
+        // R = exp(w_hat), t = A*v, where v = xi.head(3)
         R = Matrix3f::Identity(3,3)
             + sinf(norm_w) / norm_w * w_hat
             + (1-cosf(norm_w)) / (norm_w * norm_w) * w_hat*w_hat;
@@ -87,17 +87,17 @@ void convertSE3ToT(const Vector6f &xi, Matrix3f &R, Vector3f &t) {
               + (1-cosf(norm_w)) / (norm_w * norm_w) * w_hat
               + (norm_w-sinf(norm_w)) / (norm_w * norm_w *norm_w) * (w_hat * w_hat)
             ) // A matrix
-            * xi.tail(3); // * v
+            * xi.head(3); // * v
     } else {
         // R = I, A = I
         R = Matrix3f::Identity(3,3);
-        t = xi.tail(3);
+        t = xi.head(3);
     }
 }
 
 /**
  * Convert rotation matrix and translation vector to xi twist coordinates
- * @param xi output twist coordinates array of the form (w1, w2, w3, v1, v2, v3)
+ * @param xi output twist coordinates array of the form (v1, v2, v3, w1, w2, w3)
  * @param R  rotation matrix
  * @param t  translation vector
  */
@@ -110,16 +110,16 @@ void convertTToSE3(Vector6f &xi, const Matrix3f &R, const Vector3f &t) {
     if (norm_w > 0) {
         // log(R) = w_hat
         w_hat = norm_w/(2*sinf(norm_w)) * (R-R.transpose());
-        // v = xi.tail(3) = inverse(A) * t
-        xi.tail(3) = ( Matrix3f::Identity(3,3)
+        // v = xi.head(3) = inverse(A) * t
+        xi.head(3) = ( Matrix3f::Identity(3,3)
                        + (1-cosf(norm_w))*w_hat /(norm_w*norm_w)
                        + (norm_w-sinf(norm_w)) * w_hat * w_hat / (norm_w * norm_w * norm_w)
                      ).inverse() // a^-1 matrix
                      * t; // * t
     } else {
         w_hat = Matrix3f::Zero(3,3);
-        xi.tail(3) = t;
+        xi.head(3) = t;
     }
 
-    xi.head(3) << w_hat(2,1) , w_hat(0,2) , w_hat(1,0);
+    xi.tail(3) << w_hat(2,1) , w_hat(0,2) , w_hat(1,0);
 }
