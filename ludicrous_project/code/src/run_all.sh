86b7f3e149c27cb1ed290f0b7a5b4517d9fa1b6f
Guillermo Gonzalez de Garibay <g.garibay@tum.de>
2016-04-08 14:34:38 +0200

    Improve edits of run_all script

--- ludicrous_project/code/src/run_all.sh
+++ ludicrous_project/code/src/run_all.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+MYPATH="../data/rgbd_dataset_freiburg1_desk2" &&
+
 make clean && make all &&
 
-./ludicrous_non_cublas -path ../data/rgbd_dataset_freiburg1_desk2 -tDistWeights 0 > run_all_out.txt &&
+./ludicrous_non_cublas -path $MYPATH -tDistWeights 0 > run_all_out.txt &&
 
-./ludicrous_non_cublas -path ../data/rgbd_dataset_freiburg1_desk2 -tDistWeights 1  >> run_all_out.txt &&
+./ludicrous_non_cublas -path $MYPATH -tDistWeights 1  >> run_all_out.txt &&
 
-./ludicrous_cublas -path ../data/rgbd_dataset_freiburg1_desk2 -tDistWeights 0 >> run_all_out.txt &&
+./ludicrous_cublas -path $MYPATH -tDistWeights 0 >> run_all_out.txt &&
 
-./ludicrous_cublas -path ../data/rgbd_dataset_freiburg1_desk2 -tDistWeights 1  >> run_all_out.txt
+./ludicrous_cublas -path $MYPATH -tDistWeights 1  >> run_all_out.txt
