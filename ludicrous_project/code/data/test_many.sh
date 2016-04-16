8044b58a220a2391a30d2ecd0ea306aaad05d559
Guillermo Gonzalez de Garibay <g.garibay@tum.de>
2016-04-16 08:40:03 +0200

    Add README

--- ludicrous_project/code/data/test_many.sh
+++ ludicrous_project/code/data/test_many.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# This script runs all the datasets listed in datasets.txt
 while read setname; do
+	#get url component
 	if [[ $setname == *"freiburg1"* ]]
 	then
   		urlname="freiburg1/"$setname;
@@ -10,9 +12,11 @@ while read setname; do
 	then
   		urlname="freiburg3/"$setname;		
 	fi &&
+	# get file, uncommpress and delete compressed version
 	wget http://vision.in.tum.de/rgbd/dataset/$urlname.tgz &&
 	tar -xzvf $setname.tgz &&
 	rm "$setname".tgz &&
+	#copy appropriate K.txt into folder
 	if [[ $setname == *"freiburg1"* ]]
 	then
   		cp ./K_freiburg1/K.txt "$setname";
@@ -24,6 +28,7 @@ while read setname; do
   		cp ./K_freiburg3/K.txt ./"$setname";
 	fi &&
 
+	#go to src directory and run the program
 	cd ../src &&
 	./run_all.sh $setname &&
 	cd - &&
